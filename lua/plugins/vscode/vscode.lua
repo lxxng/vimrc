19f6e2796034ea4d5ecc3add60ce2c96a2478cfe
lxp <lixupeng@jasolar.com>
2026-02-28 17:39:11 +0800

    multi-cursors去掉flash相关

--- lua/plugins/vscode/vscode.lua
+++ lua/plugins/vscode/vscode.lua
@@ -16,6 +16,20 @@ return {
     {
         'vscode-neovim/vscode-multi-cursor.nvim',
         event = 'VeryLazy',
-        config = true,
+        opts = {
+            default_mappings = false,
+        },
+        config = function(_, opts)
+            local cursors = require('vscode-multi-cursor')
+            cursors.setup(opts)
+            vim.keymap.set({ 'n', 'x' }, 'mc', cursors.create_cursor, { expr = true, desc = 'Create cursor' })
+            vim.keymap.set({ 'n' }, 'mcc', cursors.cancel, { desc = 'Cancel/Clear all cursors' })
+            vim.keymap.set({ 'n', 'x' }, 'mi', cursors.start_left, { desc = 'Start cursors on the left' })
+            vim.keymap.set({ 'n', 'x' }, 'mI', cursors.start_left_edge, { desc = 'Start cursors on the left edge' })
+            vim.keymap.set({ 'n', 'x' }, 'ma', cursors.start_right, { desc = 'Start cursors on the right' })
+            vim.keymap.set({ 'n', 'x' }, 'mA', cursors.start_right, { desc = 'Start cursors on the right' })
+            vim.keymap.set({ 'n' }, '[mc', cursors.prev_cursor, { desc = 'Goto prev cursor' })
+            vim.keymap.set({ 'n' }, ']mc', cursors.next_cursor, { desc = 'Goto next cursor' })
+        end,
     },
 }
