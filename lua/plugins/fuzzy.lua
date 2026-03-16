c268b25ee932b8feb954a108864d8cc35b1c3d45
lxp <lixupeng@jasolar.com>
2026-03-16 17:26:26 +0800

    telescope live_grep 和 grep_string

--- lua/plugins/fuzzy.lua
+++ lua/plugins/fuzzy.lua
@@ -9,37 +9,46 @@ return {
             -- mingw64 make: 将 mingw32-make.exe 复制为 make.exe
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         },
+        opts = {
+            defaults = {
+                path_display = {
+                    'truncate',     -- 优先展示文件结尾
+                },
+                mappings = {
+                    i = {
+                        ['<Esc>'] = require('telescope.actions').close,
+                        ['<C-U>'] = false,
+                    },
+                },
+            },
+            pickers = {
+                find_files = { hidden = true, },
+                live_grep = { hidden = true, },
+                grep_string = { hidden = true, },
+                oldfiles = { hidden = true, },
+                git_files = { hidden = true, }
+            },
+            extensions = {
+                ['ui-select'] = {},
+                fzf = { fuzzy = true },
+            }
+        },
         config = function(_, opts)
             local telescope = require('telescope')
-            local actions = require('telescope.actions')
             local builtin = require('telescope.builtin')
-            telescope.setup({
-                defaults = {
-                    path_display = {
-                        'truncate',  -- 优先展示文件结尾
-                    },
-                    mappings = {
-                        i = {
-                            ['<Esc>'] = actions.close,
-                            ['<C-U>'] = false,
-                        },
-                    },
-                },
-                pickers = {
-                    find_files = { hidden = true, },
-                    live_grep = { hidden = true, },
-                    grep_string = { hidden = true, },
-                    oldfiles = { hidden = true, },
-                    git_files = { hidden = true, }
-                },
-                extensions = {
-                    ['ui-select'] = {},
-                    fzf = { fuzzy = true },
-                }
-            })
+            telescope.setup(opts)
 
             vim.keymap.set('n', '<leader>ff', builtin.find_files)
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+            vim.keymap.set('n', '<leader>fg', function()
+                local cword = vim.fn.expand('<cword>')
+                vim.ui.input({ prompt = 'keyword:', default = cword }, function(input)
+                    if input and input ~= '' then
+                        builtin.grep_string({ search = input })
+                    end
+                end)
+            end)
+            vim.keymap.set('x', '<leader>fg', builtin.grep_string)
+            vim.keymap.set('n', '<Leader>fG', builtin.live_grep)
             vim.keymap.set('n', 'gR', builtin.lsp_references)
             vim.keymap.set('n', 'gd', builtin.lsp_definitions)
             vim.keymap.set('n', 'gy', builtin.lsp_type_definitions)
