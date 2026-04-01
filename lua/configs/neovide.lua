fcf0e02eaebb6c61ab709aa904746d9c3e4836b7
lxp <lixupeng@jasolar.com>
2026-04-01 11:18:46 +0800

    neovide改为小狼毫配置

--- lua/configs/neovide.lua
+++ lua/configs/neovide.lua
@@ -1,27 +1,3 @@
--- 初始不允许使用输入法
-vim.g.neovide_input_ime = false
-
-local ime_input = vim.api.nvim_create_augroup('ime_input', { clear = true })
-
--- 插入模式允许使用输入法
-vim.api.nvim_create_autocmd('InsertEnter', {
-    group = ime_input,
-    pattern = "*",
-    callback = function() vim.g.neovide_input_ime = true end,
-})
-vim.api.nvim_create_autocmd('InsertLeave', {
-    group = ime_input,
-    pattern = "*",
-    callback = function() vim.g.neovide_input_ime = false end,
-})
--- 命令行模式
-vim.api.nvim_create_autocmd('CmdlineLeave', {
-    group = ime_input,
-    callback = function() vim.g.neovide_input_ime = false end,
-})
--- 允许通过<C-Space>开启输入法
-vim.keymap.set({ 'v', 'n', 'i', 'c', 't' }, '<C-Space>', function() vim.g.neovide_input_ime = true end, { noremap = true })
-
 vim.g.neovide_position_animation_length = 0
 vim.g.neovide_cursor_animation_length = 0.00
 vim.g.neovide_cursor_trail_size = 0
